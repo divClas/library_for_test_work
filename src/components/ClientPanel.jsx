2906e84042a14e487ede48bb4d9f5d9c24ec484a
divClas <afarvaev4@gmail.com>
2023-08-04 12:47:29 +0300

    ы

--- src/components/ClientPanel.jsx
+++ src/components/ClientPanel.jsx
@@ -5,54 +5,11 @@ import { useSelector, useDispatch } from 'react-redux';
 const ClientPanel = () => {
     const books = useSelector((state) => state.books);
     const dispatch = useDispatch();
-    const [newBookTitle, setnewBookTitle] = useState('');
-    const [newBookAuthor, setnewBookAuthor] = useState('');
-    const [newBookGenre, setNewBookGenre] = useState('');
-    const [newBookPublisher, setBookPublisher] = useState('');
     const [searchQuery, setSearchQuery] = useState('');
-
-    const addBook = () => {
-        const newBook = {
-            id: books.length + 1,
-            title: newBookTitle,
-            author: newBookAuthor,
-            genre: newBookGenre,
-            publisher: newBookPublisher,
-        };
-        dispatch({
-            type: "ADD_BOOK",
-            payload: newBook,
-        });
-        setnewBookTitle('');
-        setnewBookAuthor('');
-        setNewBookGenre('');
-        setBookPublisher('');
-    };
-    const deleteBook = (bookId) => {
-        dispatch({
-            type: "DELETE_BOOK",
-            payload: bookId,
-        });
-    }
-    // добавление книг
-    const handleInputChangeTitle = (event) => {
-        setnewBookTitle(event.target.value);
-    };
-
-    const handleInputChangeAuthor = (event) => {
-        setnewBookAuthor(event.target.value);
-    };
-    const handleInputChangeGenre = (event) => {
-        setNewBookGenre(event.target.value);
-    };
-    const handleInputChangePublisher = (event) => {
-        setBookPublisher(event.target.value);
-    };
     const handleSearchInputChange = (event) => {
         setSearchQuery(event.target.value);
     };
     // добавление книг
-    // поиск книг
     const filteredBooks = books.filter((book) => {
         const query = searchQuery.toLowerCase();
 
